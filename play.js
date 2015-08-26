33b0a0bf14e434c7695170915577093175c0e01b
abhishek <abhishekcode1@gmail.com>
2015-08-27 01:11:35 +0530

    moving

--- play.js
+++ play.js
@@ -27,13 +27,13 @@ var play_state = {
         //space_key.onDown.add(this.jump, this); 
 		//touch_key.onDown.add(this.jump, this);
 
-        this.pipes = game.add.group();
-        this.pipes.createMultiple(20, 'pipe');  
-        this.timer = this.game.time.events.loop(1500, this.add_row_of_pipes, this);           
-
-        this.bird = this.game.add.sprite(100, 245, 'bird');
+//        this.pipes = game.add.group();
+//        this.pipes.createMultiple(20, 'pipe');  
+//        this.timer = this.game.time.events.loop(1500, this.add_row_of_pipes, this);           
+        var x = game.world.width/2, y = game.world.height/2;
+        this.bird = this.game.add.sprite(x, y, 'bird');
        // this.bird.body.gravity.y = 1000; 
-        this.bird.anchor.setTo(-0.2, 0.5);
+        this.bird.anchor.setTo(0.5, 0.5);
         
         // Not 'this.score', but just 'score'
         score = 0; 
@@ -47,79 +47,55 @@ var play_state = {
         if (this.bird.inWorld == false)
             this.restart_game(); 
 
-        if (this.bird.angle < 20)
-            this.bird.angle += 1;
-
-        this.game.physics.overlap(this.bird, this.pipes, this.hit_pipe, null, this);      
+//        if (this.bird.angle < 20)
+//            this.bird.angle += 1;
+//
+//        this.game.physics.overlap(this.bird, this.pipes, this.hit_pipe, null, this);      
     },
 
     jump: function() {
         if (this.bird.alive == false)
             return; 
 
-        this.bird.body.velocity.y = -350;
-        this.game.add.tween(this.bird).to({angle: -20}, 100).start();
-        this.jump_sound.play();
+//        this.bird.body.velocity.y = -350;
+//        this.game.add.tween(this.bird).to({angle: -20}, 100).start();
+//        this.jump_sound.play();
     },
-
-    hit_pipe: function() {
-        if (this.bird.alive == false)
-            return;
-
-        this.bird.alive = false;
-        this.game.time.events.remove(this.timer);
-
-        this.pipes.forEachAlive(function(p){
-            p.body.velocity.x = 0;
-        }, this);
-    },
-
+    
     restart_game: function() {
+        
         this.game.time.events.remove(this.timer);
 
         // This time we go back to the 'menu' state
         this.game.state.start('menu');
-    },
-
-    add_one_pipe: function(x, y) {
-        var pipe = this.pipes.getFirstDead();
-        pipe.reset(x, y);
-        pipe.body.velocity.x = -200; 
-        pipe.outOfBoundsKill = true;
-    },
-
-    add_row_of_pipes: function() {
-        var hole = Math.floor(Math.random()*5)+1;
-        
-        for (var i = 0; i < 8; i++)
-            if (i != hole && i != hole +1) 
-                this.add_one_pipe(400, i*60+10);   
-        
-        // Not 'this.score', but just 'score'
-        score += 1; 
-        this.label_score.content = score;  
-    },
-    
-    
-    
+    },    
     moveLeft: function() {
       /// code to move left, when pressed key A
-        this.game.add.tween(this.bird).to({angle: 90}, -100).start();
+       // this.game.add.tween(this.bird).to({position:0}, 0).start();
+       if (this.bird.alive == false)
+            return; 
+        this.bird.x -=10;
         this.jump_sound.play();
     },
     moveRight: function() {
       /// code to move left, when pressed key d
-        this.game.add.tween(this.bird).to({angle: 270}, 100).start();
+       if (this.bird.alive == false)
+            return; 
+        this.bird.x +=10;
         this.jump_sound.play();
     },
     moveUp: function() {
+         if (this.bird.alive == false)
+            return; 
       /// code to move left, when pressed key w
-        this.game.add.tween(this.bird).to({angle: 0}, 100).start();
+        this.bird.y -=10;
         this.jump_sound.play();
     },
     moveDown: function() {
+         if (this.bird.alive == false)
+            return; 
       /// code to move left, when pressed key s
-        this.game.add.tween(this.bird).to({angle: 180}, -100).start();
+        this.bird.y +=10;
         this.jump_sound.play();
     },
     
